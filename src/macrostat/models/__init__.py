b71943e45d83df9d42246b3b699dc0043f61faf5
Karl Naumann-Woleske <karl@naumannwoleske.com>
2025-03-23 21:48:48 +0100

    feat: model manager to provide functions like get_model and get_model_classes

--- src/macrostat/models/__init__.py
+++ src/macrostat/models/__init__.py
@@ -1,16 +1,20 @@
-from .godleylavoie2006_SIM import (
-    SIM,
-    BehaviorSIM,
-    ParametersSIM,
-    ScenariosSIM,
-    VariablesSIM,
+from .GL06SIM import (
+    GL06SIM,
+    BehaviorGL06SIM,
+    ParametersGL06SIM,
+    ScenariosGL06SIM,
+    VariablesGL06SIM,
 )
+from .model_manager import get_available_models, get_model, get_model_classes
 
 __all__ = [
+    "get_available_models",
+    "get_model",
+    "get_model_classes",
     # Godley-Lavoie 2006 SIM model
-    "SIM",
-    "BehaviorSIM",
-    "ParametersSIM",
-    "ScenariosSIM",
-    "VariablesSIM",
+    "GL06SIM",
+    "BehaviorGL06SIM",
+    "ParametersGL06SIM",
+    "ScenariosGL06SIM",
+    "VariablesGL06SIM",
 ]
