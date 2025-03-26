a8c03cfc8ffc7cb7b56f2dffa8d02abd96a2cca4
Karl Naumann-Woleske <karl@naumannwoleske.com>
2025-03-26 20:07:24 +0100

    fix: add SIMEX to models namespace

--- src/macrostat/models/__init__.py
+++ src/macrostat/models/__init__.py
@@ -5,6 +5,13 @@ from .GL06SIM import (
     ScenariosGL06SIM,
     VariablesGL06SIM,
 )
+from .GL06SIMEX import (
+    GL06SIMEX,
+    BehaviorGL06SIMEX,
+    ParametersGL06SIMEX,
+    ScenariosGL06SIMEX,
+    VariablesGL06SIMEX,
+)
 from .model_manager import get_available_models, get_model, get_model_classes
 
 __all__ = [
@@ -17,4 +24,10 @@ __all__ = [
     "ParametersGL06SIM",
     "ScenariosGL06SIM",
     "VariablesGL06SIM",
+    # Godley-Lavoie 2006 SIMEX model
+    "GL06SIMEX",
+    "BehaviorGL06SIMEX",
+    "ParametersGL06SIMEX",
+    "ScenariosGL06SIMEX",
+    "VariablesGL06SIMEX",
 ]
